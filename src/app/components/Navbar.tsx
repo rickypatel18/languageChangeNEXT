02b1df216ec769955e8c11979f44111b56d31d94
rickypatel18 <patelricky184@gmail.com>
2025-05-12 13:05:05 +0530

    complet code

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const Navbar = () => {
+  const [locale, setLocale] = useState<string>("");
+  const router = useRouter();
+
+  useEffect(() => {
+    const cookieLocale = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("MYNEXTAPP_LOCALE="))
+      ?.split("=")[1];
+
+    if (cookieLocale) {
+      setLocale(cookieLocale);
+    } else {
+      const browserLocale = navigator.language.slice(0, 2);
+      setLocale(browserLocale);
+      document.cookie = `MYNEXTAPP_LOCALE=${browserLocale};`;
+      router.refresh();
+    }
+  }, [router]);
+
+  const changeLocale = (newLocale: string) => {
+    setLocale(newLocale);
+    document.cookie = `MYNEXTAPP_LOCALE=${newLocale};`;
+    router.refresh();
+  };
+
+  return (
+    <div className="py-3 flex items-center justify-between border-b">
+      <div className="flex items-center gap-3">
+        <button
+          onClick={() => changeLocale("en")}
+          className={`border p-2 rounded-sm ${
+            locale === "en" && "bg-white text-black"
+          }`}
+        >
+          EN
+        </button>
+        <button
+          onClick={() => changeLocale("fr")}
+          className={`border p-2 rounded-sm ${
+            locale === "fr" && "bg-white text-black"
+          }`}
+        >
+          FR
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+import { AbstractIntlMessages, useTranslations } from "next-intl";
+import { getMessages } from "next-intl/server";
+
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const messages: AbstractIntlMessages = await getMessages({ locale });
+  const title = (messages.TabTitles as { home: string })?.home;
+  // const title = messages.TabTitles?.home;
+  return {
+    title,
+  };
+}
+
+export default function Home() {
+  const t = useTranslations("HomePage");
+
+  return (
+    <main className="flex flex-col items-center gap-8">
+      <ol className="list-inside list-decimal text-sm text-center">
+        <li className="mb-2">
+          {t("list1")}
+          <code className="bg-black/[0.5]">app/page.tsx</code>
+        </li>
+        <li>{t("list2")}</li>
+      </ol>
+    </main>
+  );
+}
